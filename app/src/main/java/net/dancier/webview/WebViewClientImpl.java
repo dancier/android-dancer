ff5d2d2b27a008f0e24874a89fca520f91d57fef
Dominik Bruhn <Dominik.Bruhn@signal-iduna.de>
2022-09-30 18:10:57 +0200

    project init

---
+++ app/src/main/java/net/dancier/webview/WebViewClientImpl.java
@@ -0,0 +1,31 @@
+package net.dancier.webview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewClientImpl extends WebViewClient {
+
+    public static final String DOMAIN = "https://domoskanonos.github.io/nidoca-webcomponents/";
+
+    private Activity activity = null;
+
+    public WebViewClientImpl(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+
+        if (url.contains(DOMAIN)) return false;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        activity.startActivity(intent);
+
+        return true;
+
+    }
+
+}
